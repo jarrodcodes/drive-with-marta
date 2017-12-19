24f986cc064e024ad23d2220ae7aa5446fac22dc
Jarrod Young <jarrod.young@bettercloud.com>
2017-12-19 15:49:33 -0500

    more maps features

--- src/distances.js
+++ src/distances.js
@@ -18,20 +18,82 @@ class Distances extends Component {
 
     componentDidMount() {
 
-        let directionsService = new google.maps.DirectionsService();
-        function initMap() {
-            let userLocation = { lat: 33.850944399999996, lng: -84.3768865 };
-            let userLocationMap = new google.maps.Map(document.getElementById('map'), {
+        //Creating the Google map
+        function initAutocomplete() {
+            let map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 33.8509306, lng: -84.376941 },
                 zoom: 15,
-                center: userLocation
+                mapTypeId: 'roadmap'
             });
+
             let userLocationMarker = new google.maps.Marker({
-                position: userLocation,
-                map: userLocationMap
+                position: { lat: 33.8509306, lng: -84.376941 },
+                map: map
+            });
+
+            // Create the search box and link it to the UI element.
+            let input = document.getElementById('pac-input');
+            let searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener('bounds_changed', function () {
+                searchBox.setBounds(map.getBounds());
+            });
+
+            let markers = [];
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function () {
+                let places = searchBox.getPlaces();
+
+                if (places.length == 0) {
+                    return;
+                }
+
+                // Clear out the old markers.
+                markers.forEach(function (marker) {
+                    marker.setMap(null);
+                });
+                markers = [];
+
+                // For each place, get the icon, name and location.
+                let bounds = new google.maps.LatLngBounds();
+                places.forEach(function (place) {
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+                    let icon = {
+                        url: place.icon,
+                        size: new google.maps.Size(71, 71),
+                        origin: new google.maps.Point(0, 0),
+                        anchor: new google.maps.Point(17, 34),
+                        scaledSize: new google.maps.Size(25, 25)
+                    };
+
+                    // Create a marker for each place.
+                    markers.push(new google.maps.Marker({
+                        map: map,
+                        icon: icon,
+                        title: place.name,
+                        position: place.geometry.location
+                    }));
+
+                    if (place.geometry.viewport) {
+                        // Only geocodes have viewport.
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+                });
+                map.fitBounds(bounds);
             });
         }
 
-        initMap();
+        initAutocomplete();
+
+        let directionsService = new google.maps.DirectionsService();
 
         directionsService.route({
             origin: "Atlanta",
@@ -45,12 +107,17 @@ class Distances extends Component {
 
     render() {
         return (
-            <div>
-                <p>The direct driving time to your destination is {_.get(this.state, 'events.routes[0].legs[0].duration.text') || ''} </p>
-                <h3>My Google Maps Demo</h3>
-                <div id="map">
+            <body>
+                <div>
+                    <input id="pac-input" class="controls" type="text" placeholder="Search Box"></input>
+                    <div>
+                        <p>The direct driving time to your destination is {_.get(this.state, 'events.routes[0].legs[0].duration.text') || ''} </p>
+                        <h3>My Google Maps Demo</h3>
+                        <div id="map">
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </body>
         )
     }
 }
