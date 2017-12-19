0103de581770440f30a57164a3f052e08c8f302e
Jarrod Young <jarrod.young@bettercloud.com>
2017-12-19 14:07:23 -0500

    adding google map

--- src/distances.js
+++ src/distances.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import './maps.css';
 let google = window.google; //needed so that React will accept global values from the <script> tag
 
 class Distances extends Component {
@@ -11,9 +12,30 @@ class Distances extends Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
 
-        var directionsService = new google.maps.DirectionsService();
+    }
+
+    componentDidMount() {
+        let self = this;
+        navigator.geolocation.getCurrentPosition((response) => {
+            self.setState({events: response})
+          });
+
+        console.log(this.state)
+        let directionsService = new google.maps.DirectionsService();
+        function initMap() {
+            let userLocation = {lat: 33.850944399999996, lng: -84.3768865};
+            let userLocationMap = new google.maps.Map(document.getElementById('map'), {
+              zoom: 15,
+              center: userLocation
+            });
+            let userLocationMarker = new google.maps.Marker({
+              position: userLocation,
+              map: userLocationMap
+            });
+        }
+        initMap();
 
         directionsService.route({
             origin: "Atlanta",
@@ -25,17 +47,19 @@ class Distances extends Component {
             self.setState({ events: response })
         }
     )
+
+    
 }
 
     render() {
-      let routes = _.get(this.state, 'event')
-      let firstRoute = _.head(routes);
-      console.log(firstRoute)
         return (
-
+            <div>
             <p>The direct driving time to your destination is {_.get(this.state,'events.routes[0].legs[0].duration.text') || ''} </p>
+            <h3>My Google Maps Demo</h3>
+            <div id="map">
+            </div>
+            </div>
         )
-        
     }
 }
 
