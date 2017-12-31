deccb379975c7c11df34b9e96ef8f7192f783067
Jarrod Young <jarrodsy@gmail.com>
2017-12-31 13:50:08 -0500

    more changes

--- src/views/UserDataViewer.js
+++ src/views/UserDataViewer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import _ from 'lodash';
 class UserDataViewer extends Component {
 
     constructor(props) {
@@ -7,9 +7,11 @@ class UserDataViewer extends Component {
 
     }
 
-    componentDidMount() {
+    componentWillMount(nextProps) {
         let self = this;
-
+        if (self.props.GPS) {
+            self.forceUpdate()
+        }
     }
 
     componentDidUpdate() {
@@ -17,16 +19,18 @@ class UserDataViewer extends Component {
 
     componentWillReceiveProps(nextProps) {
         let self = this;
-        if (self.props.GPS) {
-            self.forceUpdate()
+        console.log(_.size(nextProps.GPS[0]))
+        console.log(nextProps)
+        if ((_.size(nextProps.GPS[0])) > 0) {
+           // self.forceUpdate()
         }
 }
 
     render() {
 
         let self = this;
-        console.log('I am UserDataViewer state', self.state);
-        console.log('I am UserDataViewer props', self.props);
+        //console.log('I am UserDataViewer state', self.state);
+        //console.log('I am UserDataViewer props', self.props);
 
         return (
             <div>
@@ -34,7 +38,7 @@ class UserDataViewer extends Component {
                     self.props.GPS[0] &&
 
                     <p>
-                       This is {self.props.GPS[0].length}
+                       This is {self.props.GPS[0][1]}
 </p>
                 }
             </div>
