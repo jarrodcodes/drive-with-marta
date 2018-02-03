68dd0f60a7603403ecba354dd70a1243f35fcc48
Jarrod Young <jarrodsy@gmail.com>
2018-02-03 17:00:17 -0500

    making this work

--- src/views/UserDataViewer.js
+++ src/views/UserDataViewer.js
@@ -6,73 +6,29 @@ class UserDataViewer extends Component {
 
     render() {
 
-
         let self = this;
         // console.log('I am UserDataViewer state', self.state);
         console.log('I am UserDataViewer props', self.props);
 
-        if (!self.props.MartaTimePrimary[0]) {
+        if (self.props.DriveTimeToDestination.DriveTimeToDestination) {
+
+                return (
+
+                    <div>
+                        <TravelResult {...self.props.DriveTimeToDestination} name={'Travel Time By Driving'} />
+                    </div>
+
+                )
+            
+        }
+
+        else {
 
             return (
 
-                <div>
-                    <TravelResult name= {"Jarrod"}/>
-                </div>
-
+                'Welcome to Smarta Commute!'
             )
         }
-
-        if (self.props.ClosestStationToDestination.ClosestStationToDestination) {
-
-            if (self.props.ClosestStationToDestination.ClosestStationToDestination[0] === "TOO_FAR") {
-
-                return (
-                    <div>
-                        Darn, your destination is too far from MARTA.
-</div>
-                )
-            }
-        }
-
-        if (self.props.MartaTimePrimary[0]) {
-
-            if (self.props.MartaTimePrimary[0][0] === "BUS_PRESENT") {
-
-                return ("Bus Present")
-            }
-        }
-
-
-        return (
-
-            <div>
-
-                {self.props.ClosestStationToUser &&
-                    self.props.ClosestStationToUser.ClosestStationToUser &&
-                    self.props.ClosestStationToUser.ClosestStationToUser.stationAddress &&
-                    'The nearest MARTA station to you is: ' + self.props.ClosestStationToUser.ClosestStationToUser.stationAddress + '.'
-                }
-                <br>
-                </br>
-                {self.props.DriveTimeToDestination &&
-                    self.props.DriveTimeToDestination.DriveTimeToDestination &&
-                    self.props.DriveTimeToDestination.DriveTimeToDestination[0] &&
-                    self.props.DriveTimeToDestination.DriveTimeToDestination[0].status &&
-                    self.props.DriveTimeToDestination.DriveTimeToDestination[0].status === 'OK' &&
-                    'Your direct driving time is: ' + self.props.DriveTimeToDestination.DriveTimeToDestination[0].routes[0].legs[0].duration.text + '.'
-                }
-                <br>
-                </br>
-                {self.props.MartaTimePrimary &&
-                    self.props.MartaTimePrimary[0] &&
-                    self.props.MartaTimePrimary[0][0] &&
-                    self.props.MartaTimePrimary[0][0].status &&
-                    self.props.MartaTimePrimary[0][0].status === 'OK' &&
-                    'Your MARTA transit time is: ' + self.props.MartaTimePrimary[0][0].routes[0].legs[0].duration.text + '.'
-                }
-
-            </div>
-        )
     }
 }
 
--- src/views/travelresult.js
+++ src/views/travelresult.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 
 class TravelResult extends Component {
-    constructor(name) {
+    constructor() {
     super()
-    this.name = name;
     }
-    render() {
 
+    render() {
+        let self = this;
+        console.log(self.props, "travel data")
         return (
             <div className="card" style={{ width: 18 + 'rem' }}>
-                <img className="card-img-top" src="https://maps.googleapis.com/maps/api/staticmap?size=400x400&maptype=roadmap&path=enc:kgmmEjd}aOzA@A\OjD~Bb@CNjATtEx@nCd@H@TDRDj@Jh@LrA^nAh@vAv@hAv@fJzGlJrHrIzGjAt@jAn@vErBtF`CjAn@hAv@dA|@`AdA|@jAx@nApJpPxKbQrB|C|@hAbAbAfA|@hAt@nAl@pAf@pA\tAVrAPvAJvAFtA?rNKxDMzDItM@dBAvAGlD[zZgDtAMnDQrNc@lYcAvACxABvAFtALjDd@hYbEvALvABdk@PhHF~PHvA@tAJrAXpAd@hAr@bAbAz@jAhEjHv@lA^b@b@\FFl@d@b@\DKLa@rAqC@CDKLULUXg@\g@|@gAz@kAbBwClAiCr@sApIwMr@sAf@}AZcBLgB?gBWqNGgBKgBOgBUeBy@cEuQ}z@y@gEs@iEq@iEk@kEg@mEe@kEyAyOScBYeB_@_Ba@}Ag@}Am@yAq@wAu@sAuB}C}LaQy@oAw@qAs@uAm@wAk@{Ag@}Ac@_B_@aB[cBi@iEcAcLc@kE_AoIuBeVKeBIiBCgB@gBBiBJgBPeBVcB\cB`@_BjEwOdAaEZaBTeBLgBFgB?iBEgBMgBSgBYcBa@_Bi@cBm@yAgPm\mAcCm@wAi@yAc@cBYcBQcBMgBaAmUKiBQeB[cBc@}AyAsDq@wAu@oA{@kAiAiAoA_AkAq@oAk@mKkE_DuAkAq@iAw@eA_AaAeA}@kAw@oAs@uAk@yAg@}Aa@aBYcBSeBMgBGiBAqE?a@@gG?sAB?" alt="Card image cap">
+            
+                <img className="card-img-top" src={"https://maps.googleapis.com/maps/api/staticmap?size=400x400&maptype=roadmap&path=enc:" + self.props.DriveTimeToDestination[0].routes[0].overview_polyline} alt="Card image cap">
                 </img>
                 <div className="card-body">
-                    <h5 className="card-title">{this.name.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{self.props.name}</h5>
+                    <p className="card-text">This method is going to take {self.props.DriveTimeToDestination[0].routes[0].legs[0].duration.text}.</p>
+                    <a href={"https://maps.google.com/?q= " + self.props} className="btn btn-primary">Let's go!</a>
                 </div>
             </div>
         )
